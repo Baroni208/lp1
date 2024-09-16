5b5f1c36d7105fb23ab661b0d0e533a75a8f1f76
guisiltro <hbaronisouza@gmail.com>
2024-09-16 09:47:10 -0300

    rr

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-function calcular(){
-    const numero1 = parseInt (document.getElementById("numero1").value)
-    const numero2 = parseInt (document.getElementById("numero2").value)
-    const numero3 = parseInt (document.getElementById("numero3").value)
-    const numero4 = parseInt (document.getElementById("numero4").value)
-    const resultado = document.getElementById("resultado")
-
-    let minimo = Math.min(numero1,numero2,numero3,numero4)
-    let maximo = Math.max(numero1,numero2,numero3,numero4)
-    imeroAleatorio = Math.floor(Math.random() * 100) + 1
-
-    resultado.innerText = `O minimo é ${minimo} e o maximo é ${maximo}`
+function gerarAleatorio(){
+    const numeroAleatorio = (Math.floor(Math.random * 11))
+    return numeroAleatorio
 }
+
+function jogar(){
+    const numero = gerarAleatorio()
+    const palpite = ""
+
+    do {
+        palpite = parseInt(prompt('Adivinhe o número'))
+        if (palpite != numero){
+            alert("Errou! tenta de novo")
+        }
+    }
+    while (palpite != numero)
+
+    alert(`Parabéns! O numero era: ${numero}`)
+}
+ç
+jogar()
